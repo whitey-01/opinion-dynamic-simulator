d7256aeab23998f02347755a7b045756475ab5fd
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-18 19:49:04 +0200

    Add files via upload

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -2,12 +2,12 @@ import random
 import os
 import time
 from enum import Enum
-import g
 import matplotlib.colors as mc
-import graph_utilities as gu
+import simulator.graph_utilities as gu
+import simulator.simulation_result as sr
+import simulator.simulation_configurator as sc
+import copy
 
-OUTPUT_DIR = "../output/"
-SIMULATIONS_DIR = OUTPUT_DIR + "simulations/"
 
 #return correct color for a vertex based on its opinion
 def color_map(opinion):
@@ -19,28 +19,9 @@ def color_map(opinion):
         return mc.hex2color("#43aa8b") + (1,)
 
 
-#defines two opinions update rules available
-class OpinionUpdateRule(Enum):
-    VOTER_MODEL = "voter-rule"
-    MAJORITY_DYNAMICS = "majority-dynamics"
-
-#defines an object used to configure the simulation correctly
-#holds the graph and other config parameters
-class SimulationConfigurator:
-    def __init__(self, graph: gu.Graph, bias: float, opinion_update_rule: OpinionUpdateRule, comment: str = "OPTIONAL COMMENT"):
-        self.graph = graph
-        if opinion_update_rule != OpinionUpdateRule.MAJORITY_DYNAMICS and opinion_update_rule != OpinionUpdateRule.VOTER_MODEL:
-            raise Exception("Error:- Invalid opinion update rule!")
-        self.opinion_update_rule = opinion_update_rule
-        #define how much agent are biased towards the dominant opinion
-        self.bias = bias
-        self.comment = comment
-
-
-
 #simulates the process on a graph using the configuration parameters specified in the configurator
 #returns an object containing the results of the simulaition
-def runSimulationOn(simulation_configurator: SimulationConfigurator):
+def runSimulationOn(simulation_configurator: sc.SimulationConfigurator):
 
     g: gu.Graph = simulation_configurator.graph
     # initializes the graph with the original opinion data
@@ -59,10 +40,10 @@ def runSimulationOn(simulation_configurator: SimulationConfigurator):
             g.vertex_properties["opinion"][v] = 1
         else:
             #set its opinion according to the opinion update rule choosen in the configurator
-           if simulation_configurator.opinion_update_rule == OpinionUpdateRule.MAJORITY_DYNAMICS:
+           if simulation_configurator.opinion_update_rule == sc.OpinionUpdateRule.MAJORITY_DYNAMICS:
                g = simulateMajorityDynamics(v, g)
 
-           if simulation_configurator.opinion_update_rule == OpinionUpdateRule.VOTER_MODEL:
+           if simulation_configurator.opinion_update_rule == sc.OpinionUpdateRule.VOTER_MODEL:
                g = simulateVoterModel(v, g)
 
         g.vertex_properties["opinion_color"][v] = color_map(g.vertex_properties["opinion"][v])
@@ -71,7 +52,7 @@ def runSimulationOn(simulation_configurator: SimulationConfigurator):
         evolutionMap[rounds] = (copy.copy(g.vertex_properties["opinion"]), copy.copy(g.vertex_properties["opinion_color"]))
 
     #return an object containing the informations produced during the simulation
-    return SimulationResult(evolutionMap, simulation_configurator)
+    return sr.SimulationResult(evolutionMap, simulation_configurator)
 
 
 #initializes the graph with the original opinion data
@@ -131,100 +112,21 @@ def absorptionStateReached(g: gu.Graph):
     return True
 
 
-
-
-
-
-#holds the informations produced during the simulation
-#also provides methods to save these informations
-class SimulationResult:
-    def __init__(self, evolutionMap: dict, simulation_configurator: SimulationConfigurator):
-        self.simulation_id = ts = str(time.time()).replace(".","")
-        self.rounds = len(evolutionMap) - 1
-        self.evolutionMap = evolutionMap
-        self.simulation_configurator = simulation_configurator
-        self.original_graph = simulation_configurator.graph
-
-    #generates a folder in output/simulations named with the samulation id
-    #in this folder saves an xml file of the graph, an xml file of the simulation, a .png of the graph
-    #and a folder containing png's of the graph showing the evolution of process
-    def saveSimulationData(self):
-
-        if not os.path.isdir(OUTPUT_DIR):
-            os.mkdir(OUTPUT_DIR)
-        if not os.path.isdir(SIMULATIONS_DIR):
-            os.mkdir(SIMULATIONS_DIR)
-
-        simulation_dir = SIMULATIONS_DIR + "s_" + self.simulation_id + "/"
-        os.mkdir(simulation_dir)
-        evolution_img_dir = simulation_dir + "evolution_imgs/"
-        os.mkdir(evolution_img_dir)
-
-        with open(simulation_dir + "simulation.xml", "w") as f:
-            f.write(self.getSimulationDataAsXML())
-
-        self.original_graph.save(simulation_dir + "graph.xml")
-
-        #0%, 25%, 50%, 75%, 100%
-        steps = [0]
-        steps.append(int(self.rounds / 100 * 25))
-        steps.append(int(self.rounds / 100 * 50))
-        steps.append(int(self.rounds / 100 * 75))
-        steps.append(self.rounds)
-
-
-        layout = gu.sfdp_layout(self.original_graph)
-        gu.graph_draw(self.original_graph,
-                      pos=layout,
-                      vertex_text=self.original_graph.vertex_index,
-                      vertex_text_color=(1, 1, 1, 1),
-                      edge_color=(1, 1, 1, 0.7),
-                      output=simulation_dir + "graph.png",
-                      output_size=(1600, 1600),
-                      adjust_aspect=False,
-                      bg_color=(0.09411764705882353, 0.11372549019607843, 0.15294117647058825, 1))
-
-        # print graph evolution in .png
-        for step in steps:
-            opinion = self.evolutionMap[step][0]
-            opinion_color = self.evolutionMap[step][1]
-            gu.graph_draw(self.original_graph,
-                          pos= layout,
-                          vertex_color=(1,1,1,0),
-                          vertex_fill_color=opinion_color,
-                          vertex_text=opinion,
-                          vertex_text_color=(1,1,1,1),
-                          edge_color=(1,1,1,0.7),
-                          output=evolution_img_dir + "round-" + str(step) + ".png",
-                          output_size=(1600,1600),
-                          adjust_aspect=False,
-                          bg_color=(0.09411764705882353, 0.11372549019607843, 0.15294117647058825,1))
-
-
-
-    def printSimulationData(self):
-        print("")
-        print("-------Simulation-------")
-        print("Simulation with id " + self.simulation_id)
-        print("The opinion update rule choosen was " + self.simulation_configurator.opinion_update_rule.value)
-        print("The bias towards the dominant opinion was " + str(self.simulation_configurator.bias))
-        print("Reaching the absorption state took " + str(self.rounds) + " rounds")
-
-    def getSimulationDataAsXML(self):
-        xmlContent = "<simulation>\n"
-        comment = "    <simulation-comment>" + self.simulation_configurator.comment + "</simulation-comment>\n"
-        id = "    <simulation-id>" + self.simulation_id + "</simulation-id>\n"
-        dynamic = "    <simulation-update-rule>" + self.simulation_configurator.opinion_update_rule.value + "</simulation-update-rule>\n"
-        bias = "    <simulation-bias>" + str(self.simulation_configurator.bias) + "</simulation-bias>\n"
-        rounds = "    <simulation-rounds>" + str(self.rounds) + "</simulation-rounds>\n"
-        return xmlContent + comment + id + dynamic + bias + rounds + "</simulation>"
-
-
 #runs a simulation (same graph and configuration) multiple times
 #returns a list of SimulationResult objects
-def runMultipleSimulations(config: SimulationConfigurator, repetitions: int):
+def runMultipleSimulations(config: sc.SimulationConfigurator, repetitions: int):
     simulations = []
     g: gu.Graph = config.graph
     for i in range(0,repetitions):
         simulations.append(runSimulationOn(config,g))
     return simulations
+
+#save as xml the results of multiple simulation on the same configuration
+def saveMultipleSimulationsData(directory: str, simulations: list):
+    simulations_tag = "<simulations>\n"
+    for simulation in simulations:
+        simulation: sr.SimulationResult = simulation
+        simulation_tag = "    <simulation-" + simulation.simulation_id + ">" + str(simulation.rounds) + "</simulation-" + simulation.simulation_id + ">\n"
+        simulations_tag += simulation_tag
+    simulations_tag += "/<simulations>"
+
