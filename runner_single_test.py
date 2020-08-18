4c5f1375fe3d9e521dc565cfec24b640a9d89d51
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-18 18:00:30 +0200

    Add files via upload

--- runner_single_test.py
+++ runner_single_test.py
@@ -4,17 +4,20 @@ import graph_utilities as gu
 
 #simple main that generate a graph and test the simulator
 
-#graph = gu.generateHypercubeGraph(d=4, draw=False)
-#graph = gu.generateKCliqueGraph(15, False)
-graph = gu.generateKCycleGraph(10, True)
+#graph = gu.generateHypercubeGraph(4)
+#graph = gu.generateKCliqueGraph(15)
+graph = gu.generateKCycleGraph(60)
 
-#config = sim.SimulationConfigurator(graph=graph, bias=0.55, opinion_update_rule=sim.OpinionUpdateRule.VOTER_MODEL, comment="Clique di 15 nodi")
-#simulationResult: sim.SimulationResult = sim.runSimulationOn(config)
+config = sim.SimulationConfigurator(graph=graph,
+                                    bias=0.35,
+                                    opinion_update_rule=sim.OpinionUpdateRule.MAJORITY_DYNAMICS,
+                                    comment="Ciclo di 60 nodi")
+simulationResult: sim.SimulationResult = sim.runSimulationOn(config)
 
 #print simulation data such as rounds, configuration parameters ecc..
-#simulationResult.printSimulationData()
+simulationResult.printSimulationData()
 
 #save all informations about simulation
 #save png's of the graph showing the diffusion of the opinion at rounds: 0, 25%, 50%, 75%, 100%
 #save in output/simulations
-#simulationResult.saveSimulationData()
+simulationResult.saveSimulationData()
