aa923f336f66aec77006b38ccac0499387d90c21
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-24 19:54:47 +0200

    Add files via upload

---
+++ python/simulator/simulator.py
@@ -0,0 +1,113 @@
+import random
+import os
+import time
+from enum import Enum
+import matplotlib.colors as mc
+import simulator.graph_generator as gg
+import simulator.simulation_result as sr
+import simulator.simulation_configurator as sc
+import graph_tool.all as gt
+import copy
+import xml.dom.minidom
+
+#return correct color for a vertex based on its opinion
+def color_map(opinion):
+    if opinion == 0:
+        #red
+        return mc.hex2color("#f3722c") + (1,)
+    else:
+        #green
+        return mc.hex2color("#43aa8b") + (1,)
+
+
+#simulates the process on a graph using the configuration parameters specified in the configurator
+#returns an object containing the results of the simulaition
+def runSimulationOn(simulation_configurator: sc.SimulationConfigurator):
+
+    g: gt.Graph = simulation_configurator.graph
+    # initializes the graph with the original opinion data
+    g = init_properties(g)
+
+    #map that stores the evolution of the graph during rounds saving its properties in a tuple
+    evolutionMap = {}
+    evolutionMap[0] = (copy.copy(g.vertex_properties["opinion"]), copy.copy(g.vertex_properties["opinion_color"]))
+
+    rounds = 0
+    while (not absorptionStateReached(g)):
+        #select UAR a vertex from graph g
+        v: gt.Vertex = g.vertex(random.randint(0, len(g.get_vertices()) - 1))
+        #set its opinion to 1 with probability specified in the configurator
+        if random.uniform(0, 1) <= simulation_configurator.bias:
+            g.vertex_properties["opinion"][v] = 1
+        else:
+            #set its opinion according to the opinion update rule choosen in the configurator
+           if simulation_configurator.opinion_update_rule == sc.OpinionUpdateRule.MAJORITY_DYNAMICS:
+               g = simulateMajorityDynamics(v, g)
+
+           if simulation_configurator.opinion_update_rule == sc.OpinionUpdateRule.VOTER_MODEL:
+               g = simulateVoterModel(v, g)
+
+        g.vertex_properties["opinion_color"][v] = color_map(g.vertex_properties["opinion"][v])
+        rounds += 1
+
+        evolutionMap[rounds] = (copy.copy(g.vertex_properties["opinion"]), copy.copy(g.vertex_properties["opinion_color"]))
+
+    #return an object containing the informations produced during the simulation
+    return sr.SimulationResult(evolutionMap, simulation_configurator)
+
+
+#initializes the graph with the original opinion data
+def init_properties(g: gt.Graph):
+    # sets properties used to keep trace of the opinion of each vertex
+    opinion = g.new_vertex_property("int", 0)
+    g.vertex_properties["opinion"] = opinion
+
+    # used for graphic rapresentation
+    opinion_color = g.new_vertex_property("vector<double>")
+    g.vertex_properties["opinion_color"] = opinion_color
+
+    for v in g.vertices():
+        g.vertex_properties["opinion_color"][v] = color_map(0)
+    return g
+
+
+
+#simulates the Voter model update rule
+def simulateVoterModel(v: gt.Vertex, g: gt.Graph):
+    neighbors = list(v.all_neighbors())
+    u: gt.Vertex = random.choice(neighbors)
+    g.vertex_properties["opinion"][v] = g.vertex_properties["opinion"][u]
+    g.vertex_properties["opinion_color"][v] = color_map(g.vertex_properties["opinion"][v])
+    return g
+
+
+#simulates the Majority dynamics update rule
+def simulateMajorityDynamics(v: gt.Vertex, g: gt.Graph):
+    opinion0_counter = 0
+    opinion1_counter = 0
+    for vertex in v.all_neighbors():
+        if g.vertex_properties["opinion"][vertex] == 0:
+            opinion0_counter += 1
+        else:
+            opinion1_counter += 1
+
+    if opinion0_counter > opinion1_counter:
+        g.vertex_properties["opinion"][v] = 0
+        g.vertex_properties["opinion_color"][v] = color_map(0)
+        return g
+    if opinion1_counter > opinion0_counter:
+        g.vertex_properties["opinion"][v] = 1
+        g.vertex_properties["opinion_color"][v] = color_map(1)
+        return g
+
+    g.vertex_properties["opinion"][v] = random.choice([0,1])
+    g.vertex_properties["opinion_color"][v] = color_map(g.vertex_properties["opinion"][v])
+    return g
+
+
+#checks if the process has reached the absorption state
+def absorptionStateReached(g: gt.Graph):
+    for vertex in g.vertices():
+        if g.vertex_properties["opinion"][vertex] == 0:
+            return False
+    return True
---
+++ python/simulator/tester.py
@@ -0,0 +1,68 @@
+import simulator.graph_generator as gg
+import graph_tool.all as gt
+import simulator.simulation_configurator as sc
+import simulator.simulator as sim
+import xml.dom.minidom
+import os
+import time
+
+#a Test is an execution of multiple simulation on the same graph and configuration
+
+OUTPUT_DIR = "output/"
+TESTS_DIR = OUTPUT_DIR + "tests/"
+
+#returns a list of SimulationResult objects
+def runTest(config: sc.SimulationConfigurator, repetitions: int):
+    simulations = []
+    g: gt.Graph = config.graph
+    for i in range(0,repetitions):
+        simulations.append(sim.runSimulationOn(config))
+        print("simulation " + str(i) + " processed")
+    return simulations
+
+
+#save as xml the results of multiple simulations on the same configuration
+def saveTestDataAsXML(config:sc.SimulationConfigurator, simulations: list):
+    if not os.path.isdir(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    if not os.path.isdir(TESTS_DIR):
+        os.mkdir(TESTS_DIR)
+
+    testID = str(time.time()).replace(".", "")
+    testDir = TESTS_DIR + "/t_" + testID + "/"
+    os.mkdir(testDir)
+
+    config.graph.save(testDir + "graph.xml")
+    gt.graph_draw(config.graph,
+                  vertex_text=config.graph.vertex_index,
+                  vertex_text_color=(1, 1, 1, 1),
+                  edge_color=(1, 1, 1, 0.7),
+                  output=testDir + "graph.png",
+                  output_size=(1600, 1600),
+                  adjust_aspect=False,
+                  bg_color=(0.09411764705882353, 0.11372549019607843, 0.15294117647058825, 1))
+
+    test = "<test>"
+    test += "<test-id>" + testID + "</test-id>"
+    test += "<!-- Number of simulations executed during the test -->"
+    test += "<test-repetitions>" + str(len(simulations)) + "</test-repetitions>"
+    test += "<!-- Test configuration shared by simulations -->"
+    test += "<test-config>" + config.configXMLSerializer() + "</test-config>"
+    average_rounds = 0;
+    for simulation in simulations:
+        simulation: sr.SimulationResult = simulation
+        simulation_tag = "<simulation>" \
+                            "<simulation-id>" + simulation.simulation_id + "</simulation-id>"  \
+                            "<simulation-rounds>" + str(simulation.rounds) + "</simulation-rounds>" \
+                         "</simulation>"
+        test += simulation_tag
+        average_rounds += simulation.rounds
+
+    average_rounds = int(average_rounds / len(simulations))
+    test += "<!-- average rounds needed to reach absorbing state -->"
+    test += "<test-average-rounds>" + str(average_rounds) + "</test-average-rounds>"
+    test += "</test>"
+    dom = xml.dom.minidom.parseString(test)
+    pretty_xml_as_string = dom.toprettyxml()
+    with open(testDir + "test_result.xml", "w") as f:
+        f.write(pretty_xml_as_string)
