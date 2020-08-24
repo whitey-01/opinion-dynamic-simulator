27b334510daaf25d6792fd62208f5320eec8a133
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-24 20:10:27 +0200

    Update runner_test.py

--- python/runner_test.py
+++ python/runner_test.py
@@ -18,7 +18,3 @@ simulations = ts.runTest(config, ITERATIONS)
 
 #file_name without extension, separated optional param directory
 ts.saveTestDataAsXML(config=config, simulations=simulations)
-
-
-
-#PROVA DI SINCRONIZZAZIONE
