3e4fb62f7908a71cded9835e31fc35cd66984c8e
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-25 17:11:39 +0200

    update

--- python/simulator/simulation_result.py
+++ python/simulator/simulation_result.py
@@ -1,28 +1,26 @@
-import simulator.simulation_configurator as sc
+import python.simulator.simulation_configurator as sc
 import time
 import os
 import graph_tool.all as gt
 import xml.dom.minidom
 
-
-
 OUTPUT_DIR = "output/"
 SIMULATIONS_DIR = OUTPUT_DIR + "simulations/"
 
 
-#holds the informations produced during the simulation
-#also provides methods to save these informations
+# holds the information produced during the simulation
+# also provides methods to save these information
 class SimulationResult:
     def __init__(self, evolutionMap: dict, simulation_configurator: sc.SimulationConfigurator):
-        self.simulation_id = str(time.time()).replace(".","")
+        self.simulation_id = str(time.time()).replace(".", "")
         self.rounds = len(evolutionMap) - 1
         self.evolutionMap = evolutionMap
         self.simulation_configurator = simulation_configurator
         self.original_graph = simulation_configurator.graph
 
-    #generates a folder in output/simulations named with the samulation id
-    #in this folder saves an xml file of the graph, an xml file of the simulation, a .png of the graph
-    #and a folder containing png's of the graph showing the evolution of process
+    # generates a folder in output/simulations named with the simulation id
+    # in this folder saves an xml file of the graph, an xml file of the simulation, a .png of the graph
+    # and a folder containing images of the graph showing the evolution of process
     def saveSimulation(self):
 
         if not os.path.isdir(OUTPUT_DIR):
@@ -38,13 +36,12 @@ class SimulationResult:
         self.exportSimulationOutputAsXML(dir=simulation_dir)
         self.original_graph.save(simulation_dir + "graph.xml")
 
-        #0%, 25%, 50%, 75%, 100%
+        # 0%, 25%, 50%, 75%, 100%
         steps = [0]
         steps.append(int(self.rounds / 100 * 25))
         steps.append(int(self.rounds / 100 * 50))
         steps.append(int(self.rounds / 100 * 75))
         steps.append(self.rounds)
-
 
         layout = gt.sfdp_layout(self.original_graph)
         gt.graph_draw(self.original_graph,
@@ -62,17 +59,16 @@ class SimulationResult:
             opinion = self.evolutionMap[step][0]
             opinion_color = self.evolutionMap[step][1]
             gt.graph_draw(self.original_graph,
-                          pos= layout,
-                          vertex_color=(1,1,1,0),
+                          pos=layout,
+                          vertex_color=(1, 1, 1, 0),
                           vertex_fill_color=opinion_color,
                           vertex_text=opinion,
-                          vertex_text_color=(1,1,1,1),
-                          edge_color=(1,1,1,0.7),
+                          vertex_text_color=(1, 1, 1, 1),
+                          edge_color=(1, 1, 1, 0.7),
                           output=evolution_img_dir + "round-" + str(step) + ".png",
-                          output_size=(1600,1600),
+                          output_size=(1600, 1600),
                           adjust_aspect=False,
-                          bg_color=(0.09411764705882353, 0.11372549019607843, 0.15294117647058825,1))
-
+                          bg_color=(0.09411764705882353, 0.11372549019607843, 0.15294117647058825, 1))
 
     def exportSimulationOutputAsXML(self, dir):
         simulations_file = "<simulation>"
