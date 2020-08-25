354b981273a87213a233d2ba1741112d61b298b0
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-25 17:19:04 +0200

    update

--- python/runner_test.py
+++ python/runner_test.py
@@ -2,18 +2,17 @@ import python.simulator.tester as ts
 import python.simulator.simulation_configurator as sc
 import python.simulator.graph_generator as gg
 
-#simple main that performs a test by running multiple simulation on a fixed config.
+# simple main that performs a test by running multiple simulation on a fixed config.
 ITERATIONS = 50
 
 graph = gg.generateHypercubeGraph(9)
-#graph = gu.generateKCycleGraph(45)
+# graph = gu.generateKCycleGraph(45)
 config = sc.SimulationConfigurator(graph=graph,
                                    bias=0.40,
                                    opinion_update_rule=sc.OpinionUpdateRule.VOTER_MODEL)
 
-
-#returns a list of SimulationResult objects
+# returns a list of SimulationResult objects
 simulations = ts.runTest(config, ITERATIONS)
 
-#file_name without extension, separated optional param directory
+# file_name without extension, separated optional param directory
 ts.saveTestDataAsXML(config=config, simulations=simulations)
