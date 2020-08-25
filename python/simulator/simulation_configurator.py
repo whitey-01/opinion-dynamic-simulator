d706ab7d6f72b54c2248dd6e2a424800588defd1
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-25 17:53:19 +0200

    update

--- python/simulator/simulation_configurator.py
+++ python/simulator/simulation_configurator.py
@@ -2,25 +2,26 @@ import graph_tool as gt
 from enum import Enum
 
 
-#defines two opinions update rules available
+# defines two opinions update rules available
 class OpinionUpdateRule(Enum):
     VOTER_MODEL = "voter-rule"
     MAJORITY_DYNAMICS = "majority-dynamics"
 
-#defines an object used to configure the simulation correctly
-#holds the graph and other config parameters
+
+# defines an object used to configure the simulation correctly
+# holds the graph and other config parameters
 class SimulationConfigurator:
-    def __init__(self, graph: gt.Graph, bias: float, opinion_update_rule: OpinionUpdateRule, comment: str = "OPTIONAL COMMENT"):
+    def __init__(self, graph: gt.Graph, bias: float, opinion_update_rule: OpinionUpdateRule,
+                 comment: str = "OPTIONAL COMMENT"):
         self.graph = graph
         if opinion_update_rule != OpinionUpdateRule.MAJORITY_DYNAMICS and opinion_update_rule != OpinionUpdateRule.VOTER_MODEL:
             raise Exception("Error:- Invalid opinion update rule!")
         self.opinion_update_rule = opinion_update_rule
-        #defines how much agent are biased towards the dominant opinion
+        # defines how much agent are biased towards the dominant opinion
         self.bias = bias
         self.comment = comment
 
-
-    #returns a xml string of the configurator
+    # returns a xml string of the configurator
     def configXMLSerializer(self):
         config = "<config>"
         comment = "<config-comment>" + self.comment + "</config-comment>"
