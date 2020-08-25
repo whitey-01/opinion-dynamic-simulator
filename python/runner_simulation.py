b5197f14be534d96d71b3077247d9c21e413e799
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-25 17:17:27 +0200

    update

--- python/runner_simulation.py
+++ python/runner_simulation.py
@@ -3,22 +3,22 @@ import python.simulator.simulation_configurator as sc
 import python.simulator.simulation_result as sr
 import python.simulator.graph_generator as gg
 
-#simple main that performs a simulation of the process
+# simple main that performs a simulation of the process
 
 graph = gg.generateHypercubeGraph(3)
-#graph = gg.generateKCliqueGraph(28)
-#graph = gg.generateKCycleGraph(25)
+# graph = gg.generateKCliqueGraph(28)
+# graph = gg.generateKCycleGraph(25)
 
 config = sc.SimulationConfigurator(graph=graph,
-                                    bias=0.6,
-                                    opinion_update_rule=sc.OpinionUpdateRule.VOTER_MODEL)
+                                   bias=0.6,
+                                   opinion_update_rule=sc.OpinionUpdateRule.VOTER_MODEL)
 
 simulationResult: sr.SimulationResult = sim.runSimulationOn(config)
 
-#print simulation data such as rounds, configuration parameters ecc..
+# print simulation data such as rounds, configuration parameters ecc..
 simulationResult.printSimulationData()
 
-#save all informations about simulation
-#save png's of the graph showing the diffusion of the opinion at rounds: 0, 25%, 50%, 75%, 100%
-#save in output/simulations
+# save all information about simulation
+# save images of the graph showing the diffusion of the opinion at rounds: 0, 25%, 50%, 75%, 100%
+# save in output/simulations
 simulationResult.saveSimulation()
