33c23de1db68ca9461548f032eef4d58497fa916
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-08-29 15:57:12 +0200

    update

--- python/simulator/graph_generator.py
+++ python/simulator/graph_generator.py
@@ -64,7 +64,7 @@ def generateKCycleGraph(vertices_num: int):
 
 --------------- WIP --------------------
 
-# generates a random, non directed graph connected
+# generates a random, non directed, connected graph
 def generateRandomGraph(vertices_num: int, average_deg: int):
     g = gt.Graph(directed=False)
     g = addVerticesTo(g, vertices_num)
