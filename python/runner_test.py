a4bce4a82d3c1edfc76ca89f38298d3b08c15427
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-09-06 14:03:30 +0200

    update

--- python/runner_test.py
+++ python/runner_test.py
@@ -1,3 +1,5 @@
+import python.simulator.test_configurator as tc
+import python.simulator.test_result as tr
 import python.simulator.tester as ts
 import python.simulator.simulation_configurator as sc
 import python.simulator.graph_generator as gg
@@ -6,15 +8,15 @@ import python.simulator.graph_generator as gg
 ITERATIONS = 100
 
 graph = gg.generateHypercubeGraph(d=11)
-# graph = gg.generateKCycleGraph(vertices_num=1024)
-config = sc.SimulationConfigurator(graph=graph,
-                                   bias=0.5,
-                                   opinion_update_rule=sc.OpinionUpdateRule.MAJORITY_DYNAMICS,
-                                   comment="Ipercubo 2048 nodi")
 
-# returns a list of SimulationResult objects
-simulations = ts.runTest(config, ITERATIONS)
+simulationConfigurator = sc.SimulationConfigurator(graph=graph,
+                                                   bias=0.5,
+                                                   opinion_update_rule=sc.OpinionUpdateRule.MAJORITY_DYNAMICS,
+                                                   comment="Ipercubo 2048 nodi")
 
-# file_name without extension, separated optional param directory
-# on big graphs (1024+ nodes) drawing the graph can take quite some time and require a lot of power
-ts.saveTestDataAsXML(config=config, simulations=simulations, draw=False)
+testConfigurator = tc.TestConfigurator(simulationConfigurator=simulationConfigurator, iterations=ITERATIONS)
+results = ts.runTest(testConfigurator=testConfigurator)
+
+testResult = tr.TestResult(testConfigurator=testConfigurator,results=results)
+testResult.saveTest(draw=True)
+
--- python/simulator/tester.py
+++ python/simulator/tester.py
@@ -1,14 +1,15 @@
-import python.simulator.simulation_configurator as sc
+import python.simulator.test_configurator as tc
 import python.simulator.simulation_result as sr
 import python.simulator.simulator as sim
 
 
 # returns a list of dicts containing simulations id and rounds
 # without storing each time the same configuration included in simulationResults obj
-def runTest(config: sc.SimulationConfigurator, repetitions: int):
+def runTest(testConfigurator: tc.TestConfigurator):
+    iterations = testConfigurator.iterations
     simulations = []
-    for i in range(0, repetitions):
-        simulation: sr.SimulationResult = sim.runSimulationOn(config)
+    for i in range(0, iterations):
+        simulation: sr.SimulationResult = sim.runSimulationOn(testConfigurator.simulationConfigurator)
         temp = {"simulation_id": simulation.simulation_id, "simulation_rounds": simulation.rounds}
         simulations.append(temp)
         print("simulation " + str(i) + " processed")
