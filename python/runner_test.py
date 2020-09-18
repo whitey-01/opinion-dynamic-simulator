e088177377af062dc0001db6357fb2766937f9c4
whitey-01 <69690288+whitey-01@users.noreply.github.com>
2020-09-18 16:26:54 +0200

    update

--- python/runner_test.py
+++ python/runner_test.py
@@ -5,16 +5,16 @@ import python.simulator.simulation_configurator as sc
 import python.simulator.graph_generator as gg
 
 # simple main that performs a test by running multiple simulation on a fixed config.
-ITERATIONS = 100
+ITERATIONS = 10
 
-# graph = gg.generateHypercubeGraph(d=11)
-# graph = gg.generateKCliqueGraph(4096)
-graph = gg.generateKCycleGraph(1024)
+graph = gg.generateHypercubeGraph(d=10)
+# graph = gg.generateKCliqueGraph(64)
+# graph = gg.generateKCycleGraph(1024)
 
 simulationConfigurator = sc.SimulationConfigurator(graph=graph,
                                                    bias=0.25,
                                                    opinion_update_rule=sc.OpinionUpdateRule.MAJORITY_DYNAMICS,
-                                                   comment="Ciclo 1024 nodi")
+                                                   comment="Ipercubo 1024 nodi")
 
 testConfigurator = tc.TestConfigurator(simulationConfigurator=simulationConfigurator, iterations=ITERATIONS)
 
